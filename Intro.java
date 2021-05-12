a4f7eb4960506446e72679cf263c78daa2ca316b
SNIP001 <73440452+SNIP001@users.noreply.github.com>
2021-05-13 00:21:05 +0530

    Add files via upload

---
+++ Intro.java
@@ -0,0 +1,12 @@
+package $21_Errors;
+
+public class Intro {
+
+}
+//  Compile Time Errors, Logical Errors, Runtime Errors
+
+// Cmpile Time Error-----cannot find symbol, in compatible types, invalid method declaration, this cannot be used inside a static context
+//Logical Errors----int i =576;     byte b = (byte)i;
+// Runtime Errors----Exception will cause-> Abnormal termination of a program, Informal/Un Friendly information to the end user, Improper shutdown of the resources
+// in java Exception is a class
+// whenever an exception happens the jvm will create a instance of the class
